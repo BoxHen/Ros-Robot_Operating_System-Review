7d3aef3d3fd2c1235a67dcc4815461b65001106f
Henry <Hchen101@binghamton.edu>
2019-01-14 22:54:49 -0500

    pub/sub python talker

--- Publisher_Subscriber/Python/talker.py
+++ Publisher_Subscriber/Python/talker.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 # license removed for brevity
-import rospy
-from std_msgs.msg import String
+import rospy #needed for all ROS Node
+from std_msgs.msg import String #allows reuse of std_msgs/String message type for publishing.
 
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
+		#declares that your node is publishing to the chatter topic using the message type 
+		#String. String here is actually the class std_msgs.msg.String. 
+		
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
+		#tells rospy the name of your node. This is important since until rospy has this 
+		#information, it cannot communicate with the Master. Here, the node name is, talker.
+		
+    rate = rospy.Rate(10) #10Hz - loop at the rate. With 10Hz, we should expect to go through the loop 10 times per second
+    while not rospy.is_shutdown(): #check is_shutdown() to see if your program should exit (e.g. if there is a Ctrl-C etc).
         hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
@@ -18,3 +24,5 @@ if __name__ == '__main__':
         talker()
     except rospy.ROSInterruptException:
         pass
+	#rospy.ROSInterruptException exception, thrown by rospy.sleep() and rospy.Rate.sleep() when Ctrl-C is pressed 
+	#or your Node is otherwise shutdown. This happens so code does not execute after the sleep().
