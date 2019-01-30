3f95fe55acb64066ab918a1b6da98c54615612c3
BoxHen <hchen101@binghamton.edu>
2019-01-29 22:43:56 -0500

    creating ros server

---
+++ ROS Services/creating_service_area_rectangle/server.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env 
+
+import ros_service_assignment.srv from RectangleAreaService
+import ros_service_assignment.srv from RectangleAreaServiceRequest
+import ros_service_assignment.srv from RectangleAreaServiceResponse
+
+import rospy
+
+
