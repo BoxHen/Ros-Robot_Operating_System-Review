9c0af0c5b5c2edcd28d4da7e93c7b50264756f44
BoxHen <hchen101@binghamton.edu>
2019-01-30 23:51:30 -0500

    creating ros server

--- ROS Services/creating_service_area_rectangle/server.py
+++ ROS Services/creating_service_area_rectangle/server.py
@@ -6,4 +6,16 @@ import ros_service_assignment.srv from RectangleAreaServiceResponse
 
 import rospy
 
+def handle_rectangle_area(req):
+	print "The area of the rectangle is [%s + %s = %s]" %(req.width, req.height, (req.width * req.height))
+	return RectangleAreaResponse(req.width * req.height) # send back response to the client
+
+def rectangle_area_server():
+	rospy.init_node('rectangle_area_server')
+	s = rospy.Service('rectangle_area_service', RectangleArea, handle_rectangle_area)
+	print "ready to calculate area"
+	rospy.spin()
+
+if __name__ == "__main__":
+	rectangle_area_server()
 
