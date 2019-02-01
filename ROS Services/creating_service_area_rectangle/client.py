cea63dc38d4ebd0c1e43517fd61c51e657669b95
BoxHen <hchen101@binghamton.edu>
2019-02-01 00:28:25 -0500

    creating ros client

--- ROS Services/creating_service_area_rectangle/client.py
+++ ROS Services/creating_service_area_rectangle/client.py
@@ -14,3 +14,16 @@ def request_rectangle_area(x, y):
         return server_response.area
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
+
+def usage():
+    return "%s [x y]"%sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        x = float(sys.argv[1])
+        y = float(sys.argv[2])
+    else:
+        print usage()
+        sys.exit(1)
+    print "Requesting the area of a rectangle of width %s and height %s"%(x, y)
+    print "area of the   a rectangle of width %s and height %s = %s"%(x, y, request_rectangle_area(x, y))
